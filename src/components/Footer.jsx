127ea72c72485d49500fd8af4ec478d93e8fae1a
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2026-04-11 20:26:50 +0530

    UI: footer  added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer(){
+  return (
+    <footer className="py-5 text-center bg-dark-body1 border-t border-white/3">
+      <p className="text-white/40 text-xs uppercase tracking-[0.3em] font-bold">
+        Developed with ❤️ by{" "}
+        <a
+          href="https://dhinakaran-y.github.io/My-Portfolio/"
+          target="blank"
+          className="text-white/90 hover:underline">
+          Dhinakaran Y
+        </a>
+      </p>
+    </footer>
+  );
+}
